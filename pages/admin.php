2e49cb75c1afc51108b4a26861f81a0cc4dd34b0
Hen-ops <ndevacc@gmail.com>
2025-12-04 22:56:56 +0800

    minor changes in login and admin page defined products page

--- pages/admin.php
+++ pages/admin.php
@@ -3,12 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Products</title>
+    <title>Manager</title>
 </head>
 <body>
-    This is what u will see when after u login as a customer
-    <button>Okay i am customer</button>
-    <?php
-    ?>
+    this is what you see if you are a manager or administrator.
+    <button>Okay i am Administrator</button>
 </body>
 </html>
